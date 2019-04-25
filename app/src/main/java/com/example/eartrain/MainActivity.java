9df3179a81fd616d759feca7537e808b64f4d91f
Jigurd <sigurd.stapnes@gmail.dom>
2019-04-25 12:26:27 +0200

    Add SettingsActivity

--- app/src/main/java/com/example/eartrain/MainActivity.java
+++ app/src/main/java/com/example/eartrain/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eartrain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
