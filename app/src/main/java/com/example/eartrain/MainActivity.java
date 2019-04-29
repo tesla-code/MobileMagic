b5e8ab4f98af5a2bb1d5cddd2d2b7706ea18e46c
bjornkau <bjornkau@stud.ntnu.no>
2019-04-29 12:37:53 +0200

    changing to ROOM db

--- app/src/main/java/com/example/eartrain/MainActivity.java
+++ app/src/main/java/com/example/eartrain/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity
 {
     StorageHandler storageHandler;
     Button chordButton, intervalButton, statButton, replicationButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,9 +22,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         setUp();
         tutorial();
+
     }
 
     @Override
